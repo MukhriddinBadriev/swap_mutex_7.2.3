37d453a944ae0e7485513f0e5defb49486f7a803
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2023-02-10 00:05:58 +0300

    swap mutex

---
+++ 7.2.3/7.2.3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+class Data {
+public:
+    std::mutex m;
+};
+
+void lock_swap(Data* m1, Data* m2) {
+    std::lock(m1, m2);
+    std::swap(m1, m2);
+}
+
+void scoped_lock_swap(Data* m1, Data* m2) {
+    std::scoped_lock s_l{ m1,m2 };
+    std::swap(m1, m2);
+}
+
+void unique_lock_swap(Data* m1, Data* m2) {
+
+    std::swap(m1, m2);
+}
+
+int main()
+{
+    return 0;
+}
